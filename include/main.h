1f18fffa2632efbb313210e4e77a65ad76fd407e
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-16 12:26:38 +0530

    Added ship.h

--- include/main.h
+++ include/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "ship.h" 
+#include "ship_env.h" 
 
 int main(int argc, char *argv[]);
 
---
+++ include/ship.h
@@ -0,0 +1,10 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+#include "ship_env.h" 
+
+void process_operands(int argc, char *argv[]);
+void exec_action();
+
+#endif 
+
